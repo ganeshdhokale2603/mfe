8770d9a0b60fe859611e908a4715091ab7c27be0
gaaneshdhokale2603 <ganeshdhokale@gmail.com>
2022-07-23 22:35:42 +0530

    modified text again

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there -----READY</h1>
+      <h1>Hi there --GD---READY</h1>
       <hr />
       <MarketingApp />
     </div>
