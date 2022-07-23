6d525cf2fa7c7ff6963cc26ed743766c6a5ea778
gaaneshdhokale2603 <ganeshdhokale@gmail.com>
2022-07-23 18:47:42 +0530

    updated text for marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there --- GD PUNE--%%!!!!</h1>
+      <h1>Hi there --- GD PUNE--READY</h1>
       <hr />
       <MarketingApp />
     </div>
